7bc048030a96e9580a41b2cb49283268a6b47850
Brian Surface <brian.surface@outlook.com>
2023-09-02 12:36:35 -0400

    added model

---
+++ lib/Models/vaccine_model.dart
@@ -0,0 +1,11 @@
+class VaccineModel{
+  final String? vaccineName;
+  final String? maker;
+  final String? firstDate;
+  final String? secondDate;
+  final String? boosterDate;
+  final bool? isBooster;
+
+  const VaccineModel({this.maker, this.firstDate, this.secondDate, this.boosterDate, this.isBooster,this.vaccineName});
+
+}
