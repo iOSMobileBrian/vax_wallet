5c6eee39441b8ac3236e2ea1912838574e61d32d
Brian Surface <brian.surface@outlook.com>
2023-09-02 12:36:14 -0400

    add constants

---
+++ lib/Constants/vaccine_constants.dart
@@ -0,0 +1,7 @@
+class VaxConstants {
+  static const String vaxTitle = 'Vax App';
+  static const String vaccineScreenTitle = 'Vaccine Data';
+  static const String vaccineInfoTitle =
+      'Please provide your vaccine information to include the Pharmaceutical Company and dates of vaccination';
+
+}
