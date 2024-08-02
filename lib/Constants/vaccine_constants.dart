100fd6980a24fa7907f44b1f4386c1b0f52ad037
Brian Surface <brian.surface@outlook.com>
2024-08-01 22:08:39 -0400

    updates

--- lib/Constants/vaccine_constants.dart
+++ lib/Constants/vaccine_constants.dart
@@ -1,7 +1,17 @@
+import 'package:flutter/material.dart';
+
 class VaxConstants {
   static const String vaxTitle = 'Vax App';
   static const String vaccineScreenTitle = 'Vaccine Data';
   static const String vaccineInfoTitle =
       'Please provide your vaccine information to include the Pharmaceutical Company and dates of vaccination';
-
+//card colors
+  static const List<Color> cardColors = [
+    Colors.white,
+    Colors.blue,
+    Colors.grey,
+    Colors.yellow,
+    Colors.green,
+    Colors.deepOrange
+  ];
 }
--- lib/Screens/vax_wallet.dart
+++ lib/Screens/vax_wallet.dart
@@ -1,12 +1,107 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:sticky_headers/sticky_headers/widget.dart';
+
+import '../Constants/vaccine_constants.dart';
+import '../Models/vaccine_model.dart';
+import '../Providers/vaccine_provider_viewlist.dart';
 
 class VaxWallet extends StatelessWidget {
   const VaxWallet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final provider = Provider.of<VaccineProviderViewList>(context);
+    final List<VaccineModel> list = provider.vaccineList;
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: list.isNotEmpty
+          ? SafeArea(
+              minimum: const EdgeInsets.symmetric(vertical: 10),
+              top: true,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: CustomScrollView(
+                    //physics: ClampingScrollPhysics(),
+                    slivers: <Widget>[
+                      SliverList(
+                        delegate: SliverChildBuilderDelegate((context, index) {
+                          final vax = list[index];
+                          return StickyHeader(
+                              header: Stack(
+                                children: [
+                                  Container(
+                                    height: 16,
+                                    color: index > 0 ? VaxConstants.cardColors[index - 1] : Colors.transparent,
+                                  ),
+                                  InkWell(
+                                    onTap: () => provider.reorderList(index),
+                                    child: Container(
+                                      height: 40.0,
+                                      decoration: BoxDecoration(
+                                        borderRadius: const BorderRadius.only(
+                                            topRight: Radius.circular(8.0), topLeft: Radius.circular(8.0)),
+                                        color: VaxConstants.cardColors[index],
+                                      ),
+                                      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                                      alignment: Alignment.centerLeft,
+                                      child: Row(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            'Vaccine: ${vax.vaccineName}',
+                                            style: const TextStyle(color: Colors.black),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              content: index == list.length - 1
+                                  ? Container(
+                                      height: 300,
+                                      width: double.maxFinite,
+                                      decoration: BoxDecoration(
+                                        borderRadius: const BorderRadius.only(
+                                            bottomRight: Radius.circular(8.0), bottomLeft: Radius.circular(8.0)),
+                                        color: VaxConstants.cardColors[index],
+                                      ),
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        mainAxisSize: MainAxisSize.max,
+                                        children: [
+                                          Text('Maker: ${vax.maker}'),
+                                          Text('1st Dose: ${vax.firstDate}'),
+                                          Text('2nd Dose: ${vax.secondDate}'),
+                                          Text('Booster: ${vax.boosterDate}'),
+                                          Center(
+                                              child: SizedBox(
+                                                  height: 200,
+                                                  width: 200,
+                                                  child: QrImageView(
+                                                      data: 'Vaccinated Dates: ${vax.firstDate}, ${vax.secondDate}')))
+                                        ],
+                                      ),
+                                    )
+                                  : Container(
+                                      height: 20,
+                                      color: VaxConstants.cardColors[index],
+                                    ));
+                        }, childCount: list.length),
+                      ),
+                    ]),
+              ),
+            )
+          : const Center(
+              child: Text(
+                'No vaccines please add one',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+    );
   }
 }
